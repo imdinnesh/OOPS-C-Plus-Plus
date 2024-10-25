2bc7c3eb19c26a4b8c451ed9ea6371d145692234
imdinnesh <itsdinnesh@gmail.com>
2024-10-25 22:51:17 +0530

    Add C++ OOPS Quick Summary

---
+++ class30.cpp
@@ -0,0 +1,58 @@
+// C++ OOPS  Quick Summary
+/*
+
+OOPS Concepts:
+    OOPS stands for Object-Oriented Programming System.
+    OOPS is a way of writing code/programing paradigm 
+        that involves creating classes and objects.
+    We also have procedural programming, which is 
+        writing code in a procedural way.
+    
+    OOPS involves the following concepts:
+        1. Class
+        2. Object
+        3. Inheritance
+        4. Polymorphism
+        5. Abstraction
+        6. Encapsulation
+    
+    OOPS vs Procedural Programming Language
+        OOPS is more secure and easy to maintain.
+        OOPS is more readable and understandable.
+        OOPS provides reusability of code.
+        OOPS provides data hiding.
+    
+    Polymorphism:
+        Polymorphism is a Greek word that means "many-shaped".
+        Polymorphism allows us to perform a single action in 
+            different ways.
+        Polymorphism is one of the key features of OOPS.
+        Polymorphism can be achieved by function overloading 
+            and function overriding.
+    
+    Encapsulation:
+        Encapsulation is a way of wrapping data and methods 
+            into a single unit.
+        Encapsulation is a way of hiding the data from the 
+            outside world.
+        Encapsulation is a way of protecting the data from 
+            unauthorized access.
+    
+    Inheritance:
+        Inheritance is a way of creating a new class using 
+            the existing class.
+        Inheritance allows us to reuse the code.
+        Inheritance allows us to create a new class that is 
+            based on an existing class.
+
+    Abstraction:
+        Abstraction is a way of hiding the implementation 
+            details and showing only the functionality to 
+            the user.
+        Abstraction is a way of showing only the necessary 
+            details to the user.
+        Abstraction is a way of hiding the complexity of 
+            the code.
+
+
+*/
